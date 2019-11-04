d52434f7116d0575169912f80f3f591ed3aad33f
kolumr <kolumronnie@gmail.com>
2019-11-04 13:05:17 +0300

    Add toggle

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+  $(".display").hide();
+  $(".service1, .display1").click(function(){
+    $(".display1").toggle();
+    $(".service1").toggle();
+  })
+  $(".service2, .display2").click(function(){
+    $(".display2").toggle();
+    $(".service2").toggle();
+  })
+  $(".service3, .display3").click(function(){
+    $(".display3").toggle();
+    $(".service3").toggle();
+  })
+});
